e1225588742ebe6265d375e4de701703042f25a1
midCenturyDude7 <mgriffes@gmail.com>
2022-04-01 11:53:34 -0700

    Override built-in component styles in Chakra UI

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,30 +2,32 @@
 module.exports = {
   "env": {
     "browser": true,
-    "es2021": true
+    "es2021": true,
   },
   "extends": [
     "eslint:recommended",
-    "plugin:react/recommended"
+    "plugin:react/recommended",
   ],
   "parserOptions": {
     "ecmaFeatures": {
-      "jsx": true
+      "jsx": true,
     },
     "ecmaVersion": "latest",
-    "sourceType": "module"
+    "sourceType": "module",
   },
   "plugins": [
-    "react"
+    "react",
   ],
   "rules": {
     // "react/prop-types": ["ignore"],
-    "quotes": ["error", "double"],
+    "quotes": ["error", "double",],
     // we want to force semicolons
-    "semi": ["error", "always"],
+    "semi": ["error", "always",],
+    // we want to force commas
+    "comma-dangle": ["error", 2,],
     // we use 2 spaces to indent our code
-    "indent": ["error", 2],
+    "indent": ["error", 2,],
     // we want to avoid extraneous spaces
-    "no-multi-spaces": ["error"],
-  }
+    "no-multi-spaces": ["error", 2,],
+  },
 };
